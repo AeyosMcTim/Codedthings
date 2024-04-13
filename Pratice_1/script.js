f03beb8014766b73204ef723e211458f7cd47640
AeyosMcTim <158472497+AeyosMcTim@users.noreply.github.com>
2024-04-12 21:11:27 -0400

    more js pratice

---
+++ Pratice_1/script.js
@@ -0,0 +1,11 @@
+let myVar = 5;
+console.log(myVar);
+myVar += 5;
+console.log(myVar)
+const addThis = 2
+function incrementNumber(initial, incrementor) {
+    return initial + incrementor;
+
+}
+
+console.log(incrementNumber(myVar, addThis));
